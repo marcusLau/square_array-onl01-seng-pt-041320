059723cf9977faf69cad4517ea1689b341886217
Marcus Lau <lau.marcus76@gmail.com>
2020-04-15 18:12:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,13 @@
 def square_array(array)
   # your code here
+  
+  # Avoid mutating the original argument 'array' by creating an empty array
   arr=[]
-  array.each { |num| arr << num ** 2}
+  
+  
+  array.each do |num|
+    arr << num ** 2
+  
+  # array.each { |num| arr << num ** 2}
   arr
 end
